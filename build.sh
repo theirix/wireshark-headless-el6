6984cfba3b08d40da27e044a8a9e555330b8e649
theirix <theirix@gmail.com>
2017-02-21 16:58:46 +0300

    Initial

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+yum install -y sed git centos-release wget make autoconf libtool automake rpm-build gcc gcc-c++ zlib-devel glib2-devel libpcap-devel c-ares-devel flex bison
+git clone -b master-2.0 https://github.com/wireshark/wireshark ~/wireshark
+cd ~/wireshark
+patch -p1 < /srv/without-gui.patch
+./autogen.sh
+./configure --disable-wireshark
+make rpm-package
+find packaging/rpm/RPMS -name '*.rpm' | xargs -I{} cp {} /srv/
